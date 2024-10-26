8719b592f5ad21909be85ee02bb826231e6dd985
Matheus Silva <matheussgsilva@hotmail.com>
2024-10-26 20:11:15 -0300

    Create the project and Schemas

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+
+mongoose.connect('mongodb://localhost:27017')
+.then(() => {
+  const app = express();
+
+  app.listen(3001, () => {
+    console.log('🚀 Server is running on http://localhost:3001');
+  });
+})
+.catch(() => console.log('erro ao conectar ao mongo'));
+
+
