6da0de5265ca64f0e0772ff074be5896de7eae31
Matheus Silva <matheussgsilva@hotmail.com>
2024-10-28 18:25:43 -0300

    create list products by category

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import multer from 'multer';
 
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategory';
+import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 import { createProduct } from './app/useCases/products/createProducts';
 import { listProducts } from './app/useCases/products/listProducts';
 
@@ -34,9 +35,7 @@ router.get('/products', listProducts);
 router.post('/products', upload.single('image'), createProduct);
 
 // Get products by category
-router.get('/categories/:categoryId/products', (req, res) => {
-  res.send('OK');
-});
+router.get('/categories/:categoryId/products', listProductsByCategory);
 
 // List orders
 router.get('/orders', (req, res) => {
